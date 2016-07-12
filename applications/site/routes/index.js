5f2d1b416909477cee525370df8a7feb124457ed
robotomize <robotomize@sakh.com>
2016-07-12 13:53:37 +1100

    api doc #1

--- applications/site/routes/index.js
+++ applications/site/routes/index.js
@@ -12,6 +12,16 @@ router.get('/', async ctx => {
     await ctx.render('index');
 });
 
+/**
+ * Schema caching API requests to the client
+ *
+ * The client should always send "since" and not necessarily "until" and "count".
+ * The client must send the offset parameter to get the entire set of tweets.
+ * Ok, the first request should be a POST user=robotomize&since=2014-05-05.
+ * In response, the server will send to his 18 tweets.
+ * Next request should be a POST user=robotomize&since=2014-05-05&offset=1, the server will return another 18 tweets
+ */
+
 router.post('/statuses/', async ctx => {
     let twitterClient = new TwitterClient();
     let user = new User();
